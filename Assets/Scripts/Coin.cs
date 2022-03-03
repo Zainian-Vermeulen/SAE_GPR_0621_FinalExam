9fa7482f8bdcd88463340e5a0ab4ed04e4f44d4f
Zainian-Vermeulen <60093849+Zainian-Vermeulen@users.noreply.github.com>
2022-03-03 21:48:41 +0200

    Question 2: Fixed coin bug not respawning
    Fixed the bug of the coin not resetting when the player respawns.

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    [SerializeField] private Level level;
 
+    private void Start()
+    {
+       // level = GetComponent<Level>();
+        level.Reset += SetCoinsActive;
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform == Level.Instance.PlayerTranform)
@@ -14,6 +20,11 @@ public class Coin : MonoBehaviour
 
             gameObject.SetActive(false);
         }   
+    }
+
+    private void SetCoinsActive()
+    {
+        gameObject.SetActive(true);
     }
 
 }
